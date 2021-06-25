6f57eb2a5a571de5de8125b84c8cea891136b62a
acr-coder <acarerisa1@gmail.com>
2021-06-25 18:05:11 +0300

    newcommit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     env:{
-        MONGO_URI: "mongodb+srv://kegitimi:kegitimi123@cluster0.isk58.mongodb.net/deneme?retryWrites=true&w=majority"
+        MONGO_URI: "mongodb+srv://yenilik:test123@cluster0.isk58.mongodb.net/deneme?retryWrites=true&w=majority"
     }
 }
